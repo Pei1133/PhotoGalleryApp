0824528fe593951f50c4c02307ca1c5df06bce39
PeiHuang <peichunhuang1133@gmail.com>
2022-08-07 22:26:14 +0800

    [feat] Add GridCell

---
+++ ImgurAPI/View/GridCell.swift
@@ -0,0 +1,42 @@
+//
+//  GridCell.swift
+//  ImgurAPI
+//
+//  Created by Pei Huang on 2022/8/6.
+//
+
+import UIKit
+
+class GridCell: UICollectionViewCell {
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
